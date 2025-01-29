94bd690c111435efb757a84348100cc2fc083664
sachelaride42 <109919823+sachelaride42@users.noreply.github.com>
2025-01-29 15:29:46 -0400

    Adicionando exercícios JS
    E reorganizando as pastas

---
+++ Exs Lógica de Programação/app.js
@@ -0,0 +1,15 @@
+//Exercicios loops 3
+/*
+let numero = prompt('Informe um número:');
+while (numero >= 0) {
+    console.log(numero);
+    numero = numero - 1;
+}*/
+
+//Exercicios loops 4
+let numero = prompt ('Informe um numero:');
+contador = 0;
+while (contador <= numero){
+    console.log(contador);
+    contador++;
+}
