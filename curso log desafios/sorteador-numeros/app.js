22d993e274e8b0d86e93bdb46d5ad5f869a49dab
sachelaride42 <109919823+sachelaride42@users.noreply.github.com>
2025-02-27 17:18:59 -0400

    Iniciando curso de lógica com desafios

---
+++ curso log desafios/sorteador-numeros/app.js
@@ -0,0 +1,7 @@
+function sortear() {
+    let quantidade = document.getElementById('quantidade').value;
+    let de = document.getElementById('de').value;
+    let ate = document.getElementById('ate').value;
+
+    alert(`${quantidade}, ${de}, ${ate}`);
+}
